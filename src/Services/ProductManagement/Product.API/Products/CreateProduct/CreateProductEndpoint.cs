0b1dfef8d3857aebdb25f4fa45bc312763a7864a
Ekaterina Demidova <e.demidova.by@gmail.com>
2024-07-09 21:36:35 +0300

    feat: develop feature handler class with CQRS and MediatR in Product.API

---
+++ src/Services/ProductManagement/Product.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,8 @@
+﻿namespace Product.API.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, string Description, string ImageFile, decimal Price, int Quantity, Guid CreatedByUserId, DateTime CreatedDate, DateTime LastModified);
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint
+    {
+    }
+}
---
+++ src/Services/ProductManagement/Product.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Product.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, string ImageFile, decimal Price, int Quantity, Guid CreatedByUserId, DateTime CreatedDate, DateTime LastModified)
+         :IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

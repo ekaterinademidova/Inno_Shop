3bdf147464023c45b70fbd06777a36b2e81201bf
Ekaterina Demidova <e.demidova.by@gmail.com>
2024-07-13 00:25:02 +0300

    fix: edit the code style

--- src/Services/ProductManagement/ProductsAPI/Products/UpdateProduct/UpdateProductEndpoint.cs
+++ src/Services/ProductManagement/ProductsAPI/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -7,15 +7,14 @@
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapPut("/products",
-                async (UpdateProductRequest request, ISender sender) =>
-                {
-                    var command = request.Adapt<UpdateProductCommand>();
-                    var result = await sender.Send(command);
-                    var response = result.Adapt<UpdateProductResponse>();
+            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateProductResponse>();
 
-                    return Results.Ok(response);
-                })
+                return Results.Ok(response);
+            })
             .WithName("UpdateProduct")
             .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status400BadRequest)

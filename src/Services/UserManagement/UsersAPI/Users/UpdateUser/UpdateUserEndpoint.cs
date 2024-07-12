354170a190a25102492fb50fe99733b4c470cef3
Ekaterina Demidova <e.demidova.by@gmail.com>
2024-07-13 00:11:04 +0300

    feat: implement UpdateUser use-case in UsersAPI

---
+++ src/Services/UserManagement/UsersAPI/Users/UpdateUser/UpdateUserEndpoint.cs
@@ -0,0 +1,27 @@
+﻿namespace UsersAPI.Users.UpdateUser
+{
+    public record UpdateUserRequest(Guid Id, string Name);
+    public record UpdateUserResponse(bool IsSuccess);
+
+    public class UpdateUserEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/users",
+                async (UpdateUserRequest request, ISender sender) =>
+                {
+                    var command = request.Adapt<UpdateUserCommand>();
+                    var result = await sender.Send(command);
+                    var response = result.Adapt<UpdateUserResponse>();
+
+                    return Results.Ok(response);
+                })
+            .WithName("UpdateUser")
+            .Produces<UpdateUserResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Update User")
+            .WithDescription("Update User");
+        }
+    }
+}
---
+++ src/Services/UserManagement/UsersAPI/Users/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace UsersAPI.Users.UpdateUser
+{
+    public record UpdateUserCommand(Guid Id, string Name)
+        : ICommand<UpdateUserResult>;
+    public record UpdateUserResult(bool IsSuccess);
+    internal class UpdateUserCommandHandler
+        (IDocumentSession session, ILogger<UpdateUserCommandHandler> logger)
+        : ICommandHandler<UpdateUserCommand, UpdateUserResult>
+    {
+        public async Task<UpdateUserResult> Handle(UpdateUserCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateUserCommandHandler.Handle called with {@Command}", command);
+            var user = await session.LoadAsync<User>(command.Id, cancellationToken);
+
+            if (user is null)
+            {
+                throw new UserNotFoundException(command.Id);
+            }
+
+            user.Name = command.Name;
+
+            session.Update(user);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateUserResult(true);
+        }
+    }
+}

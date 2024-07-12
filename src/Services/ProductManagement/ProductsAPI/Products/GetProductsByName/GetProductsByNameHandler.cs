51c7759fecc98c4d771da36f80244a76016c2a62
Ekaterina Demidova <e.demidova.by@gmail.com>
2024-07-12 23:29:49 +0300

    feat: implement GetProductsByName use-case in ProductsAPI

---
+++ src/Services/ProductManagement/ProductsAPI/Products/GetProductsByName/GetProductsByNameHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace ProductsAPI.Products.GetProductsByName
+{
+    public record GetProductByNameQuery(string Name) : IQuery<GetProductByNameResult>;
+    public record GetProductByNameResult(IEnumerable<Product> Products);
+
+    internal class GetProductByNameQueryHandler
+        (IDocumentSession session, ILogger<GetProductByNameQueryHandler> logger)
+        : IQueryHandler<GetProductByNameQuery, GetProductByNameResult>
+    {
+        public async Task<GetProductByNameResult> Handle(GetProductByNameQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByNameQueryHandler.Handle called with {@Query}", query);
+            var products = await session.Query<Product>()
+                .Where(p => p.Name.Contains(query.Name))
+                .ToListAsync(cancellationToken);
+
+            return new GetProductByNameResult(products);
+        }
+    }
+}

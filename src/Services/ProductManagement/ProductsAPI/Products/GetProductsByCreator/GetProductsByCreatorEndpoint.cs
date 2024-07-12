e65edea74c898e8557b45e9516b9580d11b61e56
Ekaterina Demidova <e.demidova.by@gmail.com>
2024-07-12 23:47:40 +0300

    feat: implement GetProductsByCreator use-case in ProductsAPI

---
+++ src/Services/ProductManagement/ProductsAPI/Products/GetProductsByCreator/GetProductsByCreatorEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace ProductsAPI.Products.GetProductsByCreator
+{
+    //public record GetProductByCreatorRequest();
+    public record GetProductByCreatorResponse(IEnumerable<Product> Products);
+
+    public class GetProductByCreatorEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/creator/{createdByUserId}", async (Guid createdByUserId, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByCreatorQuery(createdByUserId));
+                var response = result.Adapt<GetProductByCreatorResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductByCreator")
+            .Produces<GetProductByCreatorResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Creator")
+            .WithDescription("Get Product By Creator");
+        }
+    }
+}
---
+++ src/Services/ProductManagement/ProductsAPI/Products/GetProductsByCreator/GetProductsByCreatorHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace ProductsAPI.Products.GetProductsByCreator
+{
+    public record GetProductByCreatorQuery(Guid CreatedByUserId) : IQuery<GetProductByCreatorResult>;
+    public record GetProductByCreatorResult(IEnumerable<Product> Products);
+
+    internal class GetProductByCreatorQueryHandler
+        (IDocumentSession session, ILogger<GetProductByCreatorQueryHandler> logger)
+        : IQueryHandler<GetProductByCreatorQuery, GetProductByCreatorResult>
+    {
+        public async Task<GetProductByCreatorResult> Handle(GetProductByCreatorQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCreatorQueryHandler.Handle called with {@Query}", query);
+            var products = await session.Query<Product>()
+                .Where(p => p.CreatedByUserId.Equals(query.CreatedByUserId))
+                .ToListAsync(cancellationToken);
+
+            return new GetProductByCreatorResult(products);
+        }
+    }
+}

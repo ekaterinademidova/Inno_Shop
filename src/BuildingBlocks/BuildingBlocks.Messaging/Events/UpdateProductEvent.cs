745aa733cc0c95042046923e32b17783820163bc
Ekaterina Demidova <e.demidova.by@gmail.com>
2024-08-08 23:11:11 +0300

    feat: implement microservices async communication with RabbitMQ & MassTransit

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/UpdateProductEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace BuildingBlocks.Messaging.Events
+{
+    public record UpdateProductEvent : IntegrationEvent
+    {
+        public Guid ProductId { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+        public int Quantity { get; set; } = default!;
+        public Guid CreatedByUserId { get; set; } = default!;
+        public DateTime CreatedDate { get; set; } = default!;
+        public DateTime LastModified { get; set; } = default!;
+    }
+}
